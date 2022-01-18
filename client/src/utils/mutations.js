d33341e64f333a3decf6894372f4f3a694b1d819
unknown <Quentinhnilica@gmail.com>
2022-01-18 16:49:38 -0500

    Created mutations for logging in and creating a user

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,30 @@
+import { gql } from '@apollo/client'
+
+export const CREATE_USER = gql`
+    mutation CreateUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+            email
+            password
+            }
+        }
+    }
+`
+
+export const LOGIN_USER = gql`
+    mutation Login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+            email
+            password
+        }
+        }   
+    }
+`;
+
